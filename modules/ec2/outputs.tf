510eed484fd9843a8e9096f1924c8f47c058631c
Ubuntu <ubuntu@ip-172-31-5-42.us-west-2.compute.internal>
2026-03-12 09:09:29 +0000

    fix: Add vpc_id and subnet_id pass-through outputs for Terratest

--- modules/ec2/outputs.tf
+++ modules/ec2/outputs.tf
@@ -7,3 +7,13 @@ output "instance_public_ip" {
   description = "Public IP of the EC2 instance"
   value       = aws_instance.web.public_ip
 }
+
+output "vpc_id" {
+  description = "The VPC ID"
+  value       = var.vpc_id
+}
+
+output "subnet_id" {
+  description = "The Subnet ID"
+  value       = var.subnet_id
+}
